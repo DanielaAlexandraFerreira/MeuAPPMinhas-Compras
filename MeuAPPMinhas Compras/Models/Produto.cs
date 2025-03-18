eab5bc0813a6a1061e8a984d8caf44bfaf6c21ce
Daniela Alexandra Ferreira <ferreira.danielaalexandra@gmail.com>
2025-03-18 20:20:03 -0300

    Validação da inserção de produtos e correção dos produtos duplicados na tela inicial

--- MeuAPPMinhas Compras/Models/Produto.cs
+++ MeuAPPMinhas Compras/Models/Produto.cs
@@ -3,12 +3,32 @@
 namespace MeuAPPMinhas_Compras.Models
 {
     public class Produto
-
-    { 
+    {
+        string _descricao;
+        
+        
+        
         [PrimaryKey, AutoIncrement]
     
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao {
+            get => _descricao;
+                
+            set
+            {
+                if (value == null)
+                {
+
+                    throw new Exception("Por favor, preencha a descricao");
+                }
+                _descricao = value;
+
+            }
+
+
+        }
+
+
         public double Quantidade { get; set; }
         public double Preco { get; set; }
         public double Total { get => Quantidade * Preco; }
