6fcd7af571fd2be67d6f6f1657592028eea2f97e
Daniela Alexandra Ferreira <ferreira.danielaalexandra@gmail.com>
2025-03-18 19:48:48 -0300

    Finalização do Basico do aplicativo - remover e selecionar itens da lista

--- MeuAPPMinhas Compras/Views/EditarProduto.xaml.cs
+++ MeuAPPMinhas Compras/Views/EditarProduto.xaml.cs
@@ -1,3 +1,5 @@
+using MeuAPPMinhas_Compras.Models;
+
 namespace MeuAPPMinhas_Compras.Views;
 
 public partial class EditarProduto : ContentPage
@@ -6,4 +8,32 @@ public partial class EditarProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+        try
+        {
+            Produto produto_anexado = BindingContext as Produto;
+
+            Produto p = new Produto
+            {
+                Id = produto_anexado.Id,
+                Descricao = txt_decricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+
+            await App.Db.Update(p);
+            await DisplayAlert("sucesso!", "Registro atualizado", "OK");
+            await Navigation.PopAsync();
+
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
